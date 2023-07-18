6a030f7a9e71176a0f8a95a2612cd3a97d9e81f8
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2023-07-18 12:18:17 -0300

    darkmode adicionado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
-//Seu JavaScript de validação aqui
+const darkModeButton = document.getElementById("darkModeBtn");
+const lightModeCss = document.getElementById("lightModeCss");
+const darkModeCss = document.getElementById("darkModeCss");
 
-
+darkModeButton.addEventListener("click", () => {
+  lightModeCss.disabled = !lightModeCss.disabled;
+  darkModeCss.disabled = !darkModeCss.disabled;
+});
